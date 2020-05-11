20e5a73d553f2cfbf8ebfbbba3bc55a2ce880f9c
qijiankan <admin@qq.com>
2020-05-11 10:28:33 +0800

    1.2

--- src/config/Sites.ts
+++ src/config/Sites.ts
@@ -7,6 +7,7 @@ export default class Site extends Request {
   constructor() {
     super();
     this._apiDomain = "http://127.0.0.1:3000";
+    // this._apiDomain = "http://49.233.174.197:3002";
     this._siteAlias = "noteBook";
     this._token = "";
     this._domain = "";
